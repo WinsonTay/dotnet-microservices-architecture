37d0cbf851e440d33b4d7bb251e11d71f49c4589
Winson Tay <weeshen90@gmail.com>
2022-08-28 16:15:38 +0800

    Added Discount.Grpc Services , Grpc , AutoMapper

---
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+             CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,7 @@
+using Discount.Grpc.Extensions;
+using AutoMapper;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
-
 namespace Discount.Grpc
 {
     public class Program
@@ -13,11 +15,15 @@ namespace Discount.Grpc
 
             // Add services to the container.
             builder.Services.AddGrpc();
-
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+            builder.Services.AddAutoMapper(typeof(Program));
             var app = builder.Build();
 
+            app.MigrateDatabase<Program>();
             // Configure the HTTP request pipeline.
-            app.MapGrpcService<GreeterService>();
+            //app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<DiscountService>();
+            
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
