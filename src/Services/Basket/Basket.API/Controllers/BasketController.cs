c5865ff1afafacf0a52dbb021d16c67d91ea6e78
Winson Tay <weeshen90@gmail.com>
2022-09-01 14:30:44 +0800

    Added EventBus , And Basket Publish Basket Checkout Event

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using Basket.API.Entities;
 using System.Net;
 using Basket.API.GrpcServices;
+using EventBus.Messages.Events;
+using AutoMapper;
+using MassTransit;
 
 namespace Basket.API.Controllers
 {
@@ -15,15 +18,19 @@ namespace Basket.API.Controllers
     {
         private readonly IBasketRepository _repository;
         private readonly DiscountGrpcService _discountGrpcService;
+        private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public BasketController(IBasketRepository repository , DiscountGrpcService discountGrpcService)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IMapper mapper, IPublishEndpoint publishEndpoint)
         {
             _repository = repository;
             _discountGrpcService = discountGrpcService;
+            _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
 
         }
-        [HttpGet("{userName}", Name ="GetBasket")]
-        [ProducesResponseType(typeof(ShoppingCart),(int) HttpStatusCode.OK)]
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
         {
             var basket = await _repository.GetBasket(userName);
@@ -37,7 +44,7 @@ namespace Basket.API.Controllers
             //TODO: Communicate with Discount.Grpc 
             //and Calculate latest prices of product
             //Consume Discount gRPC
-            foreach(var item in basket.Items)
+            foreach (var item in basket.Items)
             {
                 var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
                 item.Price -= coupon.Amount;
@@ -45,12 +52,37 @@ namespace Basket.API.Controllers
             return Ok(await _repository.UpdateBasket(basket));
         }
 
-        [HttpDelete("{userName}", Name ="DeleteBasket")]
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> DeleteBasket(string userName)
         {
             await _repository.DeleteBasket(userName);
             return Ok();
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult>Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            //Get Basket with Total Price
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if(basket == null)
+            {
+                return BadRequest();
+            }
+            //Publish Checkout Event to RabbitMQ
+            //To be consumed by Order Api
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+
+            eventMessage.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMessage);
+
+
+            //Delete Basket
+            await _repository.DeleteBasket(basket.UserName);
+            return Accepted();
         }
 
     }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@ using Basket.API.Repositories;
 using Microsoft.Extensions.Configuration;
 using Discount.Grpc.Protos;
 using Basket.API.GrpcServices;
+using MassTransit;
 
 namespace Basket.API
 {
@@ -17,11 +18,41 @@ namespace Basket.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAutoMapper(typeof(Program));
+
+            //gRPC discount Configuration
             builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
                                            (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
-            
             builder.Services.AddScoped<DiscountGrpcService>();
 
+
+            builder.Services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host("amqp://guest:guest@localhost:5672");
+                });
+            });
+
+            builder.Services.AddOptions<MassTransitHostOptions>()
+            .Configure(options =>
+            {
+                // if specified, waits until the bus is started before
+                // returning from IHostedService.StartAsync
+                // default is false
+                options.WaitUntilStarted = true;
+
+                // if specified, limits the wait time when starting the bus
+                options.StartTimeout = TimeSpan.FromSeconds(10);
+
+                // if specified, limits the wait time when stopping the bus
+                options.StopTimeout = TimeSpan.FromSeconds(30);
+            });
+          
+
+
+            //Redis Configuration
             builder.Services.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
